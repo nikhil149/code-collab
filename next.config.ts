d86208bb9e0c1e8aac85acc40d2ca7e3bca39a84
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2025-09-16 22:49:30 +0530

    basepath updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
   basePath: "/code-collab", // Add your subdirectory path here
   typescript: {
     ignoreBuildErrors: true,
